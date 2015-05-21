60cb2ec0fff8dd06eeb5d4fe9014ebc1cd4d3e11
Adam Cao <xiaotcao@gmail.com>
2015-05-21 11:02:07 -0700

    forgot to change indentations in tests, now it s done

--- test/integration/add_hackday_test.rb
+++ test/integration/add_hackday_test.rb
@@ -3,22 +3,22 @@ require 'test_helper'
 class AddHackdayTest < ActionDispatch::IntegrationTest
 
   test "hackday needs a title" do
-  	@new = Hackday.new(title: "")
-  	@new.save
-  	assert_not(@new.valid?)
+    @new = Hackday.new(title: "")
+    @new.save
+    assert_not(@new.valid?)
   end
 
 
   test "add hackday" do
-  	get root_url
-  	assert_template 'hackdays/index'
-  	assert_difference 'Hackday.count', 1 do
-  		post hackdays_path, hackday: {
-  												title: "test1"	
-  												}
-  	end
-  	@new = Hackday.all.last
-  	assert_redirected_to hackday_path(@new)
+    get root_url
+    assert_template 'hackdays/index'
+    assert_difference 'Hackday.count', 1 do
+      post hackdays_path, hackday: {
+                          title: "test1"  
+                          }
+    end
+    @new = Hackday.all.last
+    assert_redirected_to hackday_path(@new)
 
   end
 
